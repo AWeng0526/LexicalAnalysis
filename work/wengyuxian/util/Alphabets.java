4768f05d02e78b604c708b764bff22696927f772
AWeng0526 <1154598143@qq.com>
2020-05-08 00:21:37 +0800

    添加测试用例

--- work/wengyuxian/util/Alphabets.java
+++ work/wengyuxian/util/Alphabets.java
@@ -10,13 +10,13 @@ public class Alphabets {
             '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
             'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
             'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', '.', '+', '-', '*', '/', '&',
-            '!', '\\', '(', ')', '[', ']', '{', '}', '>', '<', '=', ';', ',', ' ', '\n', '#'));
+            '!', '\\', ':', '(', ')', '[', ']', '?', '{', '}', '>', '<', '=', ';', ',', ' ', '\n', '#'));
 
     // 关键字
     public static HashSet<String> keyWords = new HashSet<>(Arrays.asList("void", "char", "int", "float", "double",
-            "short", "long", "signed", "unsigned", "struct", "union", "enum", "typedef", "sizeof", "auto", "static",
-            "register", "extern", "const", "volatile", "return", "continue", "break", "goto", "if", "else", "switch",
-            "case", "default", "for", "do", "while"));
+            "boolean", "short", "long", "signed", "unsigned", "struct", "union", "enum", "typedef", "sizeof", "auto",
+            "static", "register", "extern", "const", "volatile", "return", "continue", "break", "goto", "if", "else",
+            "switch", "case", "default", "for", "do", "while"));
 
     // 屏蔽字段
     public static HashSet<String> shield = new HashSet<>(Arrays.asList("WHITESPACE", "NEWLINE"));
